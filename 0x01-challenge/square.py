07d06c3c7d2bbf561db7ce3d8dfe6c6af341c30b
realdezzy <realdezzy1@gmail.com>
2023-04-13 10:38:42 -0700

    square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,35 +1,33 @@
 #!/usr/bin/python3
-""" Square module"""
+""" Square module """
 
 
-class square():
-    """Square class dipicting a perfect square"""
-
+class Square():
+    """ Square class """
     width = 0
     height = 0
 
     def __init__(self, *args, **kwargs):
-        """ Initialization """
+        """ Initializer """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.width * self.height
 
-    def PermiterOfMySquare(self):
-        """ Returns perimeter of square"""
+    def permiter_of_my_square(self):
+        """ Perimiter of square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        """String representation of class"""
+        """ Square in string """
         return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
     """ Import """
-
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
