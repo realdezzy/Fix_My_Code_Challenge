f4c07a70367dbfeb4f4caabb9d4d454fcb3c5aa1
realdezzy <realdezzy1@gmail.com>
2023-04-12 17:20:16 -0700

    square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -9,6 +9,7 @@ class square():
     height = 0
 
     def __init__(self, *args, **kwargs):
+        """ Initialization """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -26,8 +27,9 @@ class square():
 
 
 if __name__ == "__main__":
+    """ Import """
 
-    s = square(width=12, height=12)
+    s = square(width=9, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
